319410fde5d2a8aaaa318ad43824bff6a30aa72a
Andreas Müller <anmuel86@gmail.com>
2016-02-26 21:16:58 +0100

    Enhance Boolean Wrapper
    * add convenience shorthand methods
    * add validation specs

--- lib/wrappers/boolean.rb
+++ lib/wrappers/boolean.rb
@@ -1,16 +1,24 @@
 module Wrappers
   class Boolean < Wrapper
-    validate :is_boolean
+    validate :boolean?
 
     def convert
       String(original_value) == 'true'
     end
 
-    def is_boolean
-      stringified_value = String(value)
+    def boolean?
+      stringified_value = String(original_value)
       unless stringified_value == 'true' || stringified_value == 'false'
         errors.add(:value, :not_a_boolean)
       end
+    end
+
+    def true?
+      return value == true
+    end
+
+    def false?
+      return value == false
     end
   end
 end
--- spec/boolean_spec.rb
+++ spec/boolean_spec.rb
@@ -19,4 +19,38 @@ describe Wrappers::Boolean do
       expect(described_class.try_convert(true)).to be true
     end
   end
+
+  describe '#valid?' do
+    it 'returns true if the input value is a real boolean' do
+      expect(described_class.new(true)).to be_valid
+    end
+
+    it 'returns true if the input value is a stringly masked boolean' do
+      expect(described_class.new('true')).to be_valid
+    end
+
+    it 'returns false for any other case' do
+      expect(described_class.new(0)).to_not be_valid
+    end
+  end
+
+  describe '#true?' do
+    it 'returns true if the internal value is true' do
+      expect(described_class.new(true)).to be_true
+    end
+
+    it 'returns false if the internal value is false' do
+      expect(described_class.new(false)).to be_false
+    end
+  end
+
+  describe '#false?' do
+    it 'returns true if the internal value is false' do
+      expect(described_class.new(true)).to_not be_false
+    end
+
+    it 'returns false if the internal value is true' do
+      expect(described_class.new(false)).to be_false
+    end
+  end
 end
