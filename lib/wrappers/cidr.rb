17b812800c9b7ed0d6d73c761c69bc319f367985
Andreas Müller <anmuel86@gmail.com>
2016-02-26 21:50:52 +0100

    Enhance Cidr validation
    * add validation specs

--- lib/wrappers/cidr.rb
+++ lib/wrappers/cidr.rb
@@ -1,20 +1,31 @@
 module Wrappers
   class Cidr < Wrapper
+    validate :valid_cidr?
+
     def self.default_route?(cidr_string)
       new(cidr_string).default_route?
     end
 
     def convert
       begin
-        NetAddr::CIDR.create(original_value)
-      rescue ArgumentError => e
+        cidr = NetAddr::CIDR.create(original_value)
+        @valid = true
+        cidr
+      rescue ArgumentError, NetAddr::ValidationError => e
         logger.debug(e.message)
+        @valid = false
         nil
       end
     end
 
     def default_route?
       valid? && bits.zero?
+    end
+
+    private
+
+    def valid_cidr?
+      errors.add(:value, :not_a_cidr) unless @valid
     end
   end
 end
--- spec/cidr_spec.rb
+++ spec/cidr_spec.rb
@@ -17,6 +17,52 @@ describe Wrappers::Cidr do
     end
   end
 
+  describe '#valid?' do
+    it 'adds a not_a_cidr error code to value if invalid' do
+      cidr = described_class.new('256.256.0.0')
+      expect(cidr).to be_invalid
+      expect(cidr.errors[:value].join).to include 'not_a_cidr'
+    end
+
+    it 'is true for single IPs' do
+      expect(described_class.new(Faker::Internet.ip_v4_address)).to be_valid
+    end
+
+    it 'is true for CIDR formatted subnet strings' do
+      expect(described_class.new('1.0.0.0/8')).to be_valid
+    end
+
+    it 'is false for invalid IPs that may match simple regexes' do
+      expect(described_class.new('256.1.1.1')).to be_invalid
+    end
+
+    it 'is true for positive integers' do
+      expect(described_class.new(1)).to be_valid
+    end
+
+    it 'is true for 0' do
+      expect(described_class.new(0)).to be_valid
+    end
+
+    describe 'corner cases' do
+      invalid_example_data = [
+        nil,
+        '',
+        true,
+        false,
+        -1,
+        '-1',
+        '0',
+        '1',
+        2**128+1]
+      invalid_example_data.each do |example|
+        it "is false for #{example}" do
+          expect(described_class.new(example)).to be_invalid
+        end
+      end
+    end
+  end
+
   describe '.default_route?' do
     it 'returns true if the subnet prefix is 0' do
       expect(described_class.default_route?('0.0.0.0/0')).to be true
